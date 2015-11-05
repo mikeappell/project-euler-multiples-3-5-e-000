c55630832ffc5d14e1314bcd43efa928da1e17ca
mikeappell <mikeappell@gmail.com>
2015-11-05 15:11:05 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,7 @@
-# Enter your procedural solution here!
+def collect_multiples(num)
+  (3...num).select {|possible_multiple| possible_multiple % 3 == 0 || possible_multiple % 5 == 0}.compact
+end
+
+def sum_multiples(num)
+  collect_multiples(num).inject(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,15 @@
-# Enter your object-oriented solution here!
+require_relative 'multiples'
+
+class Multiples
+  def initialize(num)
+    @num = num
+  end
+
+  def collect_multiples
+    (3...@num).select {|possible_multiple| possible_multiple % 3 == 0 || possible_multiple % 5 == 0}.compact
+  end
+
+  def sum_multiples
+    collect_multiples.inject(:+)
+  end
+end
